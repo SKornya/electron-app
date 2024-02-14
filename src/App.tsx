1dc81321b13c0787026c62b580ba48ec1ee967c6
skornya <kornyakovS@gmail.com>
2024-02-14 15:00:55 +0300

    add header to main component, table scroll normal

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,17 @@ import Chart from './components/Chart/Chart';
 import Sidebar from './components/Sidebar/Sidebar';
 
 import './App.css';
+import Header from './components/Header/Header';
 
 function App() {
   return (
-    <div className="container">
-      <Sidebar />
-      <Chart />
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <Sidebar />
+        <Chart />
+      </div>
+    </>
   );
 }
 
