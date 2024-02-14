8ef2235d0efeab4fab762717df34611576ccd3ec
skornya <kornyakovS@gmail.com>
2024-02-14 09:15:04 +0300

    connect store

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from './App.tsx';
+
 import './index.css'
+
+import store from './store.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />      
+    </Provider>
   </React.StrictMode>,
-)
+);
