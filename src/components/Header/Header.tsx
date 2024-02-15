e3ceb39641a6449c9103e38e044f8a01a854486c
skornya <kornyakovS@gmail.com>
2024-02-15 15:25:40 +0300

    add buttons to header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,14 +5,36 @@ import ConnectSettings from "../ConnectSettings/ConnectSettings";
 import settingsIcon from '../../assets/icons/settings_icon.svg';
 
 import './Header.less';
+import { useSelector } from "react-redux";
+import { RootState } from "../../store";
 
 const Header: FunctionComponent = () => {
   const [showModal, setShowModal] = useState<boolean>(false);
   
+  const appState = useSelector((state: RootState) => state.app.state);
+
+  const startDraw = () => {
+
+  };
+
+  const PauseDraw = () => {
+
+  };
+
+  const StopDraw = () => {
+
+  };
+
   return (
     <>
       {showModal && <ConnectSettings closeModal={() => setShowModal(false)} />}
       <div className="header">
+        <div className="header__buttons">
+          <button className="header__buttons-start" disabled={appState === 'running'}>Start</button>
+          <button className="header__buttons-pause" disabled={appState !== 'running'}>Pause</button>
+          <button className="header__buttons-stop" disabled={appState === 'stopped'}>Stop</button>
+        </div>
+        
         <div className="header__settings" onClick={() => setShowModal(true)}>
           <img src={settingsIcon} alt="settings-logo" className="header__settings-logo" />
         </div>
