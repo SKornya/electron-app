7f2b13de9fa191961060711ac8761fb1caec50ea
skornya <kornyakovS@gmail.com>
2024-02-16 15:48:50 +0300

    make settings window appers inside chart container

--- src/components/ChartWrapper/ChartWrapperr.tsx
+++ src/components/ChartWrapper/ChartWrapperr.tsx
@@ -1,27 +1,52 @@
-import { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent, useRef, useState } from 'react';
 
 import Settings from '../Settings/Settings';
 import Chartjs from '../Chart/Chart';
 
-import './Chart.less';
+import './ChartWrapper.less';
 
 const ChartWrapper: FunctionComponent = () => {
 
   const [show, setShow] = useState<boolean>(false);
   const [position, setPosition] = useState({ x: 0, y: 0 });
 
-  const hideSettings = () => {
-    setShow(false);
+  const settingsRef = useRef<HTMLDivElement>(null);
+
+  const coordsCalculate = (event: React.MouseEvent<HTMLDivElement, MouseEvent>): { x: number, y: number } => {
+    const CONTAINER_WIDTH = 300;
+    const CONTAINER_HEIGHT = 300;
+
+    const spaceRight: number = window.innerWidth - event.clientX;
+    const spaceBottom: number = window.innerHeight - event.clientY;
+
+    const x = spaceRight < CONTAINER_WIDTH ? event.clientX - CONTAINER_WIDTH : event.clientX;
+    const y = spaceBottom < CONTAINER_HEIGHT ? event.clientY - CONTAINER_HEIGHT : event.clientY;
+  
+    return { x, y };
   };
 
-  const showSettings = (e: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+  const hideSettings = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+    if (show) {
+      setShow(false);
+      
+      const coords = coordsCalculate(event);
+      const { x, y } = coords;
+      setPosition({ x, y });
+    }
+  };
+
+  const showSettings = (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
+
+    const coords = coordsCalculate(event);
+    const { x, y } = coords;
+    setPosition({ x, y });
+    
     setShow(!show);
-    setPosition({ x: e.clientX, y: e.clientY });
   };
 
   return (
     <>
-      {show && <Settings setYAxis={console.log} top={position.y} left={position.x} />}
+      {show && <Settings ref={settingsRef} top={position.y} left={position.x} />}
 
       <div className='chart'
         onClick={hideSettings}
