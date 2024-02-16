7fe2298ded9578167721f6f583392b5037850026
skornya <kornyakovS@gmail.com>
2024-02-16 15:45:28 +0300

    log

--- electron/index.ts
+++ electron/index.ts
@@ -98,7 +98,10 @@ app.whenReady().then(() => {
   // for applications and their menu bar to stay active until the user quits
   // explicitly with Cmd + Q.
   app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') app.quit();
+    if (process.platform !== 'darwin') {
+      console.log('quit');
+      app.quit();
+    }
   });
 });
 
